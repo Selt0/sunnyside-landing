53be0ba46879ca22a5ede6f897c4f369fde0e2c0
Michael M <chewedgum12@gmail.com>
2022-06-13 19:06:47 -0500

    Added ability to hide mobile menu when clicked outside

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navMenu = document.querySelector('.nav-menu')
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', e => {
 	navMenu.classList.toggle('active')
+})
+
+document.addEventListener('click', e => {
+	if (
+		e.target.id != 'toggle' &&
+		navMenu.classList.contains('active') &&
+		!e.target.classList.contains('nav-menu')
+	) {
+		console.log('fired!')
+		navMenu.classList.toggle('active')
+	}
 })
