1279e424ec3c01daafdb498513806f45c08cf05e
Michael M <chewedgum12@gmail.com>
2022-06-13 19:12:59 -0500

    typo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navMenu = document.querySelector('.nav-menu')
 
-navToggle.addEventListener('click', e => {
+navToggle.addEventListener('click', () => {
 	navMenu.classList.toggle('active')
 })
 
